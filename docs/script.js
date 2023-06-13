3fae6887b93b7cad2517908501c44d59b48d7c05
Achim Tack <AchimTack@users.noreply.github.com>
2023-06-13 10:23:53 +0200

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -2,22 +2,27 @@ const canvas = document.getElementById('gameCanvas');
 const context = canvas.getContext('2d');
 let countdown = 100;
 let countdownInterval = null;
+let isRunning = false;  // New variable
 
-context.font = "30px 'Press Start 2P'";
-context.fillStyle = "red";
+context.font = "40px 'Press Start 2P'";
+context.fillStyle = "#42f545";
 context.textAlign = "center";
 context.fillText("Press SPACE to start", canvas.width / 2, canvas.height / 2);
 
 function startCountdown() {
+    if (isRunning) return;  // Don't start if already running
+    isRunning = true;  // Set running state
+
     countdownInterval = setInterval(function() {
         context.clearRect(0, 0, canvas.width, canvas.height);
-        context.font = "80px 'Press Start 2P'";  // Larger font size for countdown
+        context.font = "180px 'Press Start 2P'";  // Larger font size for countdown
         context.fillText(countdown, canvas.width / 2, canvas.height / 2);
         
         if (countdown > 0) {
             countdown--;
         } else {
             clearInterval(countdownInterval);
+            isRunning = false;  // Reset running state
         }
     }, 1000);
 }
@@ -25,8 +30,10 @@ function startCountdown() {
 function resetCountdown() {
     clearInterval(countdownInterval);
     countdown = 100;
+    isRunning = false;  // Reset running state
+
     context.clearRect(0, 0, canvas.width, canvas.height);
-    context.font = "30px 'Press Start 2P'";  // Smaller font size for start message
+    context.font = "40px 'Press Start 2P'";  // Smaller font size for start message
     context.fillText("Press SPACE to start", canvas.width / 2, canvas.height / 2);
 }
 
