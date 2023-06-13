e2b41df2f7e90a3f68306ad8d8e65cce7c5941d2
Achim Tack <AchimTack@users.noreply.github.com>
2023-06-13 11:00:11 +0200

    added touch control

--- docs/script.js
+++ docs/script.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById('gameCanvas');
 const context = canvas.getContext('2d');
 let countdown = 100;
 let countdownInterval = null;
-let isRunning = false;  // New variable
+let isStarted = false;
 
 context.font = "40px 'Press Start 2P'";
 context.fillStyle = "#42f545";
@@ -10,9 +10,6 @@ context.textAlign = "center";
 context.fillText("Press SPACE to start", canvas.width / 2, canvas.height / 2);
 
 function startCountdown() {
-    if (isRunning) return;  // Don't start if already running
-    isRunning = true;  // Set running state
-
     countdownInterval = setInterval(function() {
         context.clearRect(0, 0, canvas.width, canvas.height);
         context.font = "180px 'Press Start 2P'";  // Larger font size for countdown
@@ -22,7 +19,6 @@ function startCountdown() {
             countdown--;
         } else {
             clearInterval(countdownInterval);
-            isRunning = false;  // Reset running state
         }
     }, 1000);
 }
@@ -30,8 +26,6 @@ function startCountdown() {
 function resetCountdown() {
     clearInterval(countdownInterval);
     countdown = 100;
-    isRunning = false;  // Reset running state
-
     context.clearRect(0, 0, canvas.width, canvas.height);
     context.font = "40px 'Press Start 2P'";  // Smaller font size for start message
     context.fillText("Press SPACE to start", canvas.width / 2, canvas.height / 2);
@@ -39,8 +33,26 @@ function resetCountdown() {
 
 window.addEventListener('keydown', function(e) {
     if (e.code === 'Space') {
-        startCountdown();
+        if (!isStarted) {
+            isStarted = true;
+            startCountdown();
+        } else {
+            isStarted = false;
+            resetCountdown();
+        }
     } else if (e.code === 'Enter') {
+        isStarted = false;
         resetCountdown();
     }
 });
+
+canvas.addEventListener('touchstart', function(e) {
+    e.preventDefault();
+    if (!isStarted) {
+        isStarted = true;
+        startCountdown();
+    } else {
+        isStarted = false;
+        resetCountdown();
+    }
+}, false);
